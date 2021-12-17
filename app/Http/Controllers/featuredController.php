8fd73e20db78b982fcefb3dbc3253b6d6ba68343
MuhammedSultan1 <mfsultan01@gmail.com>
2021-12-17 18:26:07 -0500

    changed getlocation method again

--- app/Http/Controllers/featuredController.php
+++ app/Http/Controllers/featuredController.php
@@ -15,7 +15,7 @@ class featuredController extends Controller
      */
 
     public function getLocation(){
-         $clientIP = Request::ip();
+         $clientIP = request()->ip();
 
          $zipcode = Http::withHeaders([
          'x-rapidapi-host' => 'target1.p.rapidapi.com',
