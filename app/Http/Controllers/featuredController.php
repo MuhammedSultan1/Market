1eeee47c7f89bc4bd179db4751b9dbc471aa727c
MuhammedSultan1 <mfsultan01@gmail.com>
2021-12-18 18:45:53 -0500

    changed

--- app/Http/Controllers/featuredController.php
+++ app/Http/Controllers/featuredController.php
@@ -93,7 +93,7 @@ class featuredController extends Controller
         json_decode(json_encode($storeInfo),true);
 
          foreach($storeInfo as $openHours):
-            $hours = $openHours['rolling_operating_hours'];
+            $hours = $openHours['rolling_operating_hours']['regular_event_hours']['days'];
              if(isset($hours)){
                 dump($hours);
              }else{
