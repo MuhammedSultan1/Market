ee4033f47b7827c5eef87765a9b40188ee7ee2fb
MuhammedSultan1 <mfsultan01@gmail.com>
2021-12-25 19:08:32 -0500

    Changed the code that gets the user's location in the getLocation function in the featuredController. I hope this will help with the feature testing.

--- app/Http/Controllers/featuredController.php
+++ app/Http/Controllers/featuredController.php
@@ -16,25 +16,43 @@ class featuredController extends Controller
 
     public function getLocation(Request $request){
 
-         if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
-        $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
-        $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
-            }
-            $client  = @$_SERVER['HTTP_CLIENT_IP'];
-            $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
-            $remote  = $_SERVER['REMOTE_ADDR'];
+        //  if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+        // $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+        // $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+        //     }
+        //     $client  = @$_SERVER['HTTP_CLIENT_IP'];
+        //     $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+        //     $remote  = $_SERVER['REMOTE_ADDR'];
 
-            if(filter_var($client, FILTER_VALIDATE_IP)){
-                $clientIp = $client;
-            }
-            elseif(filter_var($forward, FILTER_VALIDATE_IP)){
-                $clientIp = $forward;
-            }
-            else{
-                $clientIp = $remote;
+        //     if(filter_var($client, FILTER_VALIDATE_IP)){
+        //         $clientIp = $client;
+        //     }
+        //     elseif(filter_var($forward, FILTER_VALIDATE_IP)){
+        //         $clientIp = $forward;
+        //     }
+        //     else{
+        //         $clientIp = $remote;
+        //     }
+
+                // If the application is executed by command (or unit testing).
+            if(php_sapi_name() === 'cli') {
+                return "127.0.0.1";
             }
 
-            $zipcode = Http::get('http://ip-api.com/json/'.$clientIp)->json()['zip'];
+            if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR']) {
+                // Behind balancer
+                $clientIpAddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            } else if (isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR']) {
+                // Universal PHP headers
+                $clientIpAddress = $_SERVER['REMOTE_ADDR'];
+            } else {
+                // Unusual headers
+                $clientIpAddress = $_SERVER['HTTP_CLIENT_IP'];
+            }
+
+            echo $clientIpAddress;
+
+            $zipcode = Http::get('http://ip-api.com/json/'.$clientIpAddress)->json()['zip'];
             
 
 
@@ -65,7 +83,7 @@ class featuredController extends Controller
         
             
          return view('stores',[
-             'clientIp' => $clientIp,
+             'clientIpAddress' => $clientIpAddress,
              'zipcode' => $zipcode,
              'storeList' => $storeList,
              'beginTime' => $beginTime,
