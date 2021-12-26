fe9a6f5f83ff9b930bf239a8279c41d23496cf33
MuhammedSultan1 <mfsultan01@gmail.com>
2021-12-26 00:06:59 -0500

    added location_id property to getLocation method in featuredController

--- app/Http/Controllers/featuredController.php
+++ app/Http/Controllers/featuredController.php
@@ -66,21 +66,22 @@ class featuredController extends Controller
                 
 
             //storeList variables
-            // foreach($storeList as $store):
+            foreach($storeList as $store):
             //     $beginTime = $store['rolling_operating_hours']['regular_event_hours']['days']['0']['hours']['0']['begin_time'] ?? '';
             //     $openingTime = date('h:i A', strtotime($beginTime)) ?? '';
 
             //     $endTime = $store['rolling_operating_hours']['regular_event_hours']['days']['0']['hours']['0']['end_time'] ?? '';
             //     $closingTime = date('h:i A', strtotime($endTime)) ?? '';
 
-            //     $location_id = $store['location_id'] ?? '';
-            // endforeach;
+                $location_id = $store['location_id'] ?? '';
+            endforeach;
         
             
          return view('stores',[
              'clientIpAddress' => $clientIpAddress,
              'zipcode' => $zipcode,
              'storeList' => $storeList,
+             'location_id' => $location_id,
          ]);
     }
 
